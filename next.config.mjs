d9e03333f981d9bb18afe1fc7f31f16cb4f23bfa
AhrensOG <ahrensgabriel09@gmail.com>
2025-02-16 23:08:39 +0100

    DElete comments

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // Estos son los tamaños para dispositivos, puedes ajustarlos según lo que necesites
-    deviceSizes: [640, 750, 828], 
-    // Estos tamaños son para imágenes fijas (si usas la prop "width" y "height")
+    deviceSizes: [640, 750, 828],
     imageSizes: [16, 32, 48, 64, 96],
   },
 };
